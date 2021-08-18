bd55e3a03c6a8d820691710e77c807734e0975b6
DeepakMys <89140039+DeepakMys@users.noreply.github.com>
2021-08-18 18:38:05 +0530

    Update SmartBin.ino

--- SmartBin/SmartBin.ino
+++ SmartBin/SmartBin.ino
@@ -8,8 +8,6 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 Servo servo; // Declare the name of the Servo motor
 
-String a = "USE  ME"; 
-
 #define trigPin 10 // Define the Trigger pin
 #define echoPin 9 // Define the Echo pin
  
@@ -22,6 +20,8 @@ void setup() {
   servo.attach(8); // Declare the pin in which the servo motor is attached
   pinMode(trigPin, OUTPUT); // Define that the trigPin is for output
   pinMode(echoPin, INPUT); // Define that the echoPin is for input
+  lcd.print("    USE  ME");// Print "USE ME"
+  servo.write(0);// Close the lid of the dustbin
 }
  
 void loop() {
@@ -40,12 +40,22 @@ void loop() {
   distance = (duration / 2) * 0.0343;
 
   Serial.println(distance);
-  if (distance <= 20 && distance >= 2) { // Check if the distance is lesser than 20 cm and greater than 2 cm and, if yes open the mouth of the dustbin
-    lcd.print("Thanks For Keeping Our Environment Clean"); 
+  if (distance <= 20 && distance >= 2) { // Check if the distance is lesser than 20 cm and greater than 2 cm and,
+                                         //if yes open the lid of the dustbin and print the message "Thanks For Keeping Our Environment Clean".
+    lcd.clear(); 
     servo.write(90);
-    delay(3000); 
+     delay(3000);
+     lcd.print("Thanks For Keeping Our Environment Clean");
+  for (int positionCounter = 0; positionCounter < 24; positionCounter++) {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();
+    // wait a bit:
+    delay(300);
   }
-  else { // Else close the mouth of the dustbin
+   lcd.clear();
+   lcd.print("    USE  ME");// Print "USE ME"
+  }
+  else { // Else close the lid of the dustbin
     servo.write(0);
   }
   delay(200);
