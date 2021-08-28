884e0403b91e6ae25886065f02849dcd88bf6697
DeepakMys <89140039+DeepakMys@users.noreply.github.com>
2021-08-28 15:47:17 +0530

    Update SmartBin.ino

--- SmartBin/SmartBin.ino
+++ SmartBin/SmartBin.ino
@@ -7,7 +7,7 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 Servo servo; // Declare the name of the Servo motor
-
+int pos;
 #define trigPin 10 // Define the Trigger pin
 #define echoPin 9 // Define the Echo pin
  
@@ -20,9 +20,6 @@ void setup() {
   servo.attach(8); // Declare the pin in which the servo motor is attached
   pinMode(trigPin, OUTPUT); // Define that the trigPin is for output
   pinMode(echoPin, INPUT); // Define that the echoPin is for input
-  lcd.print(" Safely Dispose");
-  lcd.setCursor(2, 1);
-  lcd.print(" Your Waste");
   servo.write(0);// Close the lid of the dustbin
 }
  
@@ -42,20 +39,31 @@ void loop() {
   distance = (duration / 2) * 0.0343;
 
   Serial.println(distance);
-  if (distance <= 20 && distance >= 2) { // Check if the distance is lesser than 20 cm and greater than 2 cm and,
-                                         //if yes open the lid of the dustbin and print the message "Thanks For Keeping Our Environment Clean".
-   lcd.clear(); 
-   servo.write(90);
-   delay(3000);
-   lcd.print("Thanks For Using");
-   delay(2000);
-   lcd.clear();
-   lcd.print(" Safely Dispose");
-   lcd.setCursor(2, 1);
-   lcd.print(" Your Waste");
+  if (distance <= 40 && distance >= 2) { // Check if the distance is lesser than 20 cm and greater than 2 cm and,
+                                         //if yes open the lid of the dustbin and print the message "Thanks For Keeping Our Environment Clean". 
+    for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 90 degrees
+      // in steps of 1 degree
+      servo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(5);                       // waits 5ms for the servo to reach the position
+    }
+    lcd.clear();
+    delay(2500);
+    for (pos = 90; pos >= 0; pos -= 1) { // goes from 90 degrees to 0 degrees
+      // in steps of 1 degree
+      servo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(5);                       // waits 5ms for the servo to reach the position
+    }
+    delay(1000);
+    lcd.print("Thanks For Using");
+    delay(1000);
+    lcd.clear();
   }
   else { // Else close the lid of the dustbin
     servo.write(0);
+    lcd.print(" Safely Dispose");
+    lcd.setCursor(2, 1);
+    lcd.print(" Your Waste");
+    delay(500);
+    lcd.clear();
   }
   delay(200);
-}
